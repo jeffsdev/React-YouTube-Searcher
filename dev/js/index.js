197e3e333406685aa766adc1ffb0dbc5fcfd1a09
Jeff Seymour <seym1585@gmail.com>
2016-08-22 22:13:21 -0700

    add youtube api function to consume data

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = "AIzaSyDVkXaPoLqzo9Pb8bbzPzzGYFOa30k5Y5U";
 
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'potion seller'}, (videos) => {
+      this.setState({ videos });
+      // the above is the same as: this.setState({ video: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".root"));

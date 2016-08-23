9b2654cee04d4b8f200e212184b47dc34bfe4ad2
Jeff Seymour <seym1585@gmail.com>
2016-08-22 19:21:41 -0700

    add initial project files

---
+++ dev/js/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+require("../../scss/style.scss");
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>React simple starter</div>
+    );
+  }
+}

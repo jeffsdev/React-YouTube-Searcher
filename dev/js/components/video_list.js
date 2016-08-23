a40e68a281d0f314b971f8632dd069cd63ae62db
Jeff Seymour <seym1585@gmail.com>
2016-08-22 22:37:08 -0700

    include video list items in video list component

--- dev/js/components/video_list.js
+++ dev/js/components/video_list.js
@@ -3,9 +3,15 @@ import VideoListItem from './video_list_item';
 
 
 const VideoList = (props) => {
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
+
   return (
     <ul className="video-list">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };

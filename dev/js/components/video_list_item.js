5c57183dd56e14545f11040317c138aef93e4456
Jeff Seymour <seym1585@gmail.com>
2016-08-23 18:20:27 -0700

    add content to video list items from API

--- dev/js/components/video_list_item.js
+++ dev/js/components/video_list_item.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  // const video = props.video; same thing as ({video}) above, rather than (props)
+
+  const imageURL = video.snippet.thumbnails.default.url;
+
+  return (
+
+    <li className="">
+
+      <div className="video-list">
+        <div className="">
+          <img className="media-object" src={imageURL}/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+
+    </li>
+
+  );
 };
 
 export default VideoListItem;
